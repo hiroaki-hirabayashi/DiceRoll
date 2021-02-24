c26f10517327b7f2dddb2e5be7f8b25c7579fc99
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2021-02-24 14:27:55 +0900

    ダイスをランダムで表示

--- Swift DiceRoll/ViewController.swift
+++ Swift DiceRoll/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    
+    @IBOutlet private weak var diceImageView1: UIImageView!
+    @IBOutlet private weak var diceImageView2: UIImageView!
+    
+    private var leftDiceNumber = 1
+    private var rightDiceNumber = 5
+    private var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0 ... 5)]
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+        print(Int.random(in: 1...10))
 
+    }
+    
 }
 
